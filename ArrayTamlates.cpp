ae1feba4d36b760e5084c5a0b388d58c9bed3a22
student <student@7PC5>
2024-02-26 19:13:06 +0300

    Добавьте файлы проекта.

---
+++ ArrayTamlates.cpp
@@ -0,0 +1,55 @@
+﻿// ArrayTamlates.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <random>
+using namespace std;
+
+/// <summary>
+/// Заполнение массива
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <param name="arr"></param>
+/// <param name="size"></param>
+template<typename T>
+void FillArray(T arr[], int size);
+
+/// <summary>
+/// Печать массива
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <param name="arr"></param>
+/// <param name="size"></param>
+template<typename T>
+void PrintArray(T arr[], int size);
+
+int main()
+{
+    
+    const int sizze = 20;
+    int Array[sizze];
+    FillArray(Array, sizze);
+    PrintArray(Array, sizze);
+}
+
+
+
+template<typename T>
+void FillArray(T arr[], int size) {
+    random_device random;
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = random() % (100 + 50) - 50;
+    }
+}
+
+template<typename T>
+void PrintArray(T arr[], int size) {
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
