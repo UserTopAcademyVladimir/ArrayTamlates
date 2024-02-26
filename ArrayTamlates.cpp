1f527aa39bb690414e61e918d310f2f6f9b09046
student <student@7PC5>
2024-02-26 20:20:37 +0300

    Пузырьковая сортировка

--- ArrayTamlates.cpp
+++ ArrayTamlates.cpp
@@ -24,6 +24,15 @@ void FillArray(T arr[], int size);
 template<typename T>
 void PrintArray(T arr[], int size);
 
+template <typename T>
+void SortBubble(T arr[], int size);
+
+template <typename T>
+bool SortBubbleLeft(T arr[], int size, int i, bool flag);
+
+template <typename T>
+bool SortBubbRight(T arr[], int size, int i, bool flag);
+
 int main()
 {
     SetConsoleCP(1251);
@@ -33,6 +42,10 @@ int main()
     int Array[sizze];
     FillArray(Array, sizze);
     PrintArray(Array, sizze);
+
+	cout << " -------------After sorting------------ " << endl;
+	SortBubble(Array, sizze);
+	PrintArray(Array, sizze);
 }
 
 
@@ -55,3 +68,49 @@ void PrintArray(T arr[], int size) {
 
     cout << endl;
 }
+
+
+template <typename T>
+void SortBubble(T arr[], int size ) {	
+	int i = 0;
+	bool flag = true;
+	for (; i < size; ++i) {		
+		
+		if (SortBubbleLeft(arr,  size, i, flag))
+			break;
+		
+		if (SortBubbRight(arr, size, i, flag))
+			break;
+	}
+}
+
+template<typename T>
+bool SortBubbleLeft(T arr[], int size, int i, bool flag){
+	for (int j = i; j < size - 1 - i; ++j) {
+		if (arr[j] > arr[j + 1])
+		{
+			int tmp = arr[j];
+			arr[j] = arr[j + 1];
+			arr[j + 1] = tmp;
+			flag = false;
+		}
+	}
+
+	return flag;
+}
+
+template<typename T>
+bool SortBubbRight(T arr[], int size, int i, bool flag) {
+	for (int k = size - 1 - i; k > i; --k) {
+		if (arr[k - 1] > arr[k])
+		{
+			int tmp = arr[k - 1];
+			arr[k - 1] = arr[k];
+			arr[k] = tmp;
+			flag = false;
+		}
+	}
+
+	return flag;
+}
+
